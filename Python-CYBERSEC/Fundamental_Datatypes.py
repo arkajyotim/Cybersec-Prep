32fb2839120e5bea523c470c55fbbbf1c9fd7d16
Arkajyoti Mukhopadhyay <arkajyoti.mukhopadhyay2016@gmail.com>
2024-11-11 22:01:51 +0530

    done

--- Python-CYBERSEC/Fundamental_Datatypes.py
+++ Python-CYBERSEC/Fundamental_Datatypes.py
@@ -1,9 +1,24 @@
-#Fundamental Data types
+                                          #Fundamental Data types
 
-#1) int -> integer . Basic mathemetical operations can easily be performed
-# print (1+2);
+
+# int -> integer . Basic mathemetical operations can easily be performed
+# print (type(1+2));  // int type class
 # print (1-2);
 # print (1*2);
-# print (1/2);
+# print (1/2);       // float data type = any kind of deciaml output (more memory)
+
+#-----------------------------------------------------------------------------------------------------------------------------
+
+                                          # Math Functions
+
+#print(round(2.6)) -> Round off to the closest integer
+#print(abs(-100)) -> prints absolute values/no negative numbers
+
+#-----------------------------------------------------------------------------------------------------------------------------
+
+                                          # Operator Precedence
+
+#Also called operator priority
+
 
 
